f6e3df01c129a04dd21855e6c45ee4ad14bf29dc
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-14 21:10:00 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    const data = [];
+function main() {
+    }
+    });
+}
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+        console.log("Random Number: " + item);
+
+    data.forEach(item => {
+
+}
+    const data = generateRandomData();
+main();
+
+function generateRandomData() {
