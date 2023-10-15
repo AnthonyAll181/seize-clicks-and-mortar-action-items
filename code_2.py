8052d90ed33708dbaee2480e723b4334657789cd
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-16 00:12:04 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,66 +1,14 @@
-    }
-        int[] data = new int[10];
-
-    public static void main(String[] args) {
-
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-}
-        }
-        }
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;            System.out.println("Random Number: " + item);
-        }
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
-
-    public static void main(String[] args) {
-        for (int item : data) {
-    }
-
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-}
-
-    for item in data:
-if __name__ == "__main__":
-
-    data = generate_random_data()
-def main():
 def generate_random_data():
-        print(f"Random Number: {item}")
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+    for item in data:
+
+
 
     return data
-    main()    data.forEach(item => {
-function generateRandomData() {
-main();
-    }
-    });
-    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-
-function main() {
-}
-
-}
-        console.log("Random Number: " + item);
-    const data = generateRandomData();
-    const data = [];
-    for (let i = 0; i < 10; i++) {    }
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-
-
-public class RandomDataGenerator {
-        for (int item : data) {
-    public static void main(String[] args) {
-}
-        }
-        }
-            System.out.println("Random Number: " + item);
+def main():
+import random
+        print(f"Random Number: {item}")
+    main()
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
